ae5973c73618b5017edbed5f29beb517e33434bc
farismusic <44246682+farismusic@users.noreply.github.com>
2018-10-18 11:59:17 +0200

    Sada sam unio broj

--- src/ba/unsa/etf/rpr/predavanje03/Main.java
+++ src/ba/unsa/etf/rpr/predavanje03/Main.java
@@ -1,8 +1,12 @@
 package ba.unsa.etf.rpr.predavanje03;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	//Ovo je neki komentar
+        System.out.println("Unesi broj n: ");
+        Scanner ulaz=new Scanner(System.in);
+        int n=ulaz.nextInt();
+
     }
 }
