ea3e3b433b7a4a758e8bedd8974240e55ae223e0
farismusic <44246682+farismusic@users.noreply.github.com>
2018-10-18 12:13:55 +0200

    Zavrsio sam zadatak

--- src/ba/unsa/etf/rpr/predavanje03/Main.java
+++ src/ba/unsa/etf/rpr/predavanje03/Main.java
@@ -3,10 +3,25 @@ import java.util.Scanner;
 
 public class Main {
 
+    public static int sumaCifara(int n){
+        int suma=0, c;
+        while(n!=0){
+            c=n%10;
+            suma+=c;
+            n/=10;
+        }
+        return suma;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Unesi broj n: ");
+        System.out.print("Unesi broj n: ");
         Scanner ulaz=new Scanner(System.in);
         int n=ulaz.nextInt();
+        for(int i=1; i<=n; i++){
+            if(i%sumaCifara(i)==0){
+                System.out.println(i);
+            }
+        }
 
     }
 }
